7e160381d171c1cde6a82fa3cbe348ac370980f2
Dominik Wernberger <dominik.wernberger@gmx.de>
2022-09-13 21:23:02 +0200

    Add section describing how to add new entries and adapt Python script

--- README.md
+++ README.md
@@ -18,3 +18,24 @@ Some non OSHW files are provided in
 ## Other Lists
 
 - [KiCad Test-Boards](https://gitlab.com/kicad/code/kicad/-/wikis/Test-Boards)
+
+## Setup
+
+### Install Linux Requirements
+
+```bash
+sudo apt install git git-lfs
+```
+
+### Install Python Requirements
+
+```bash
+python -m pip install -r requirements.txt
+```
+
+### Adding new Entries to the Database
+
+1. Fork the repository and use the naming `<author>_<name>`
+2. Modify `sql_db.py` to add your designs
+3. Run `python sql_db.py` to add the entries
+4. Run `python repos_to_md.py` to regenerate the markdown table
--- sql_db.py
+++ sql_db.py
@@ -19,13 +19,47 @@ def create_table(connection: sqlite3.Connection) -> None:
     connection.commit()
 
 
+def add_new_entry(connection: sqlite3.Connection, name: str, author: str, fork_url: str, orig_url: str) -> None:
+
+    cursor = connection.cursor()
+
+    sql_command = f"""
+        SELECT id, name, author FROM repositories
+        WHERE orig_url = "{orig_url}";
+    """
+
+    cursor.execute(sql_command)
+    results = cursor.fetchall()
+
+    if len(results) > 0:
+        print(f"Repository for URL {orig_url} already exists, not adding it to database!")
+        return
+
+    sql_command = f"""
+        INSERT INTO repositories (name, author, fork_url, orig_url)
+        VALUES ("{name}", "{author}", "{fork_url}", "{orig_url}"); 
+    """
+
+    cursor.execute(sql_command)
+
+    connection.commit()
+
+
 if __name__ == "__main__":
     connection = sqlite3.connect("repos.db")
 
     # create_table(connection)
 
+    # Add new entry
+    name = "CadenceLibrary"
+    author = "XiaoSenLuo"
+    fork_url = "https://github.com/Werni2A/XiaoSenLuo_CadenceLibrary"
+    orig_url = "https://github.com/XiaoSenLuo/CadenceLibrary"
+    add_new_entry(connection, name, author, fork_url, orig_url)
+
     cursor = connection.cursor()
 
+    # Print all entries
     cursor.execute("SELECT * FROM repositories")
     repos = cursor.fetchall()
 
